fd073504fb71f722efbccbb65c5b1a18e3b8ee33
Oskar Alfredsson <oskar.alfredsson@outlook.com>
2024-01-04 18:21:06 +0100

    Added some things

--- CV.py
+++ CV.py
@@ -14,20 +14,23 @@ def index():
 
 @app.route("/projekt")
 def project():
-   	return render_template("projects.html", projects=projects)
+   	return render_template("projects.html", projects=projects, title="Projekt")
 
 
 @app.route("/projekt/<projectTitle>")
 def projectPage(projectTitle):
-	return render_template("project-site.html", title="project")
+	return render_template("project-site.html", title=projectTitle)
 
 	abort(404)
-
-
 
 @app.route("/kontakt")
 def socials():
    	return render_template("socials.html", title="Sociala Medier")
+
+@app.route("/Om mig")
+def about():
+   	return render_template("about.html", title="Om mig")
+
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CV-site
+
+# What you need
+projects.py file where you store your projects and info about them, for an example:
+
+'projects = [
+
+		{
+	   'title': 'Website',
+       'github': 'https://github.com/Alfredsson418/CV-site',
+	   'p': 'This website',
+	   'color': 'rgb(75, 100, 200)',
+	   'done' : 'Ongoing'
+	}
+]
+'
