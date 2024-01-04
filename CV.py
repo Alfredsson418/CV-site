013384c1d912d60fd2b3527a7d9e80c2e6feb7d9
Oskar Alfredsson <oskar.alfredsson@outlook.com>
2024-01-04 01:35:43 +0100

    Started on new website

--- CV.py
+++ CV.py
@@ -1,30 +1,8 @@
 from flask import Flask, render_template, abort, url_for
+from projects import projects
 
 app = Flask(__name__)
-
-
-projects = [
-	{
-	   'title': 'Morse Code Translator',
-	   'p': 'A python script to translate morse code',
-	   'color': 'rgb(75, 127, 255)',
-	   'done' : 'Fullbordad'
-	},
-		{
-	   'title': 'Websida',
-	   'p': 'Backend och Frontend',
-	   'color': 'rgb(75, 100, 200)',
-	   'done' : 'Inte Startad'
-	},
-	{
-		'title': 'Test',
-		'p': 'Information',
-		'color': 'tomato',
-		'done': 'Nej'
-	
-	}
-]
-
+app.config['DEBUG'] = True
 
 
 
@@ -36,20 +14,19 @@ def index():
 
 @app.route("/projekt")
 def project():
-   	return render_template("project.html", title="Projekt", projects=projects)
+   	return render_template("projects.html", projects=projects)
 
 
 @app.route("/projekt/<projectTitle>")
 def projectPage(projectTitle):
-	for project in projects:
-		if projectTitle == project.get("title"):
-			return render_template("projectTemp.html", project=project)
+	return render_template("project-site.html", title="project")
 
 	abort(404)
 
-@app.route("/sociala-medier")
-def socials():
 
+
+@app.route("/kontakt")
+def socials():
    	return render_template("socials.html", title="Sociala Medier")
 
 
--- static/js/top-titlepage.js
+++
@@ -1,3 +0,0 @@
-document.write("<div class=\"web-title-container title-text-border\" >");
-document.write("<h1>Oskar Alfredsson</h1>");
-document.write("</div>");
